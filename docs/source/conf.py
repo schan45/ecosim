0dca3fcd05014094b7b6b5f77bf09fe00e23b31a
schan45 <schleier.anna@gmail.com>
2025-05-10 16:15:35 +0200

    Create conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Ecosim Toolbox: Animal Ecosystem Simulation'
+copyright = '2025, Anna Schleier, Anikó Vitos, Tamás Bence Tóth'
+author = 'Anna Schleier, Anikó Vitos, Tamás Bence Tóth'
+release = '2025. 05. 09.'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
